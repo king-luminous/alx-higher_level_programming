27012a59fbc5e565c599d88a74016477a136c715
king-luminous <albashir97@gmail.com>
2024-03-13 07:48:42 -0700

    task 10

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def add(a, b)
-
-    """
-      Function for addition of a and b and return the result
-    """
-
+    """ Function for addition of a and b and return the result """
     return (a + b)
