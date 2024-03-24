73734e025ba27030dfeec555f3f47a74623400b2
king-luminous <albashir97@gmail.com>
2024-03-24 06:02:25 -0700

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    my_sentence = ()
     for i in len(sentence):
         if i == 0:
-            return "none"
+            my_sentence = 0, "none"
         else:
-            if i = len(sentence):
-                return i, sentence[0]
+            my_sentence = len(sentence), sentence[0]
+            return my_sentence
 
