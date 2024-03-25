b2d88b69d2d35d36d31f9c7801ecf508ca642eef
king-luminous <albashir97@gmail.com>
2024-03-25 08:03:15 -0700

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_l = []
     for i in range(len(my_list)):
-        if my_l[i] % 2 == 0:
+        if my_list[i] % 2 == 0:
             new_l.append(True)
         else:
             new_l.append(False)
